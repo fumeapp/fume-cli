6655f6fdbf4ad8b68b4a7047e1a42531626c249f
acidjazz <acidjazz@gmail.com>
2020-05-27 14:23:28 -0500

    ✨ proper init command with tests

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,4 +1,4 @@
-import {Command} from '@oclif/command'
+import Command, {flags} from '@oclif/command'
 import cli from 'cli-ux'
 import * as inquirer from 'inquirer'
 import yml = require('js-yaml')
@@ -7,17 +7,30 @@ import fs = require('fs')
 export default class Init extends Command {
   static description = 'Initialize your fume project'
 
-  async run() {
-    const project = await cli.prompt('What is your project name?')
-    const responses: any = await inquirer.prompt([{
-      name: 'environment',
-      message: 'choose environments',
-      type: 'checkbox',
-      choices: [{name: 'staging'}, {name: 'production'}],
-    }])
+  static flags = {
+    name: flags.string({
+      description: 'Project Name (sets default environments)',
+      required: false,
+    }),
+  }
 
-    this.log(`Project is  ${project}`)
-    this.log(`Stage: ${responses.environment}`)
+  async run() {
+    const {flags} = this.parse(Init)
+    let name
+    let environments
+    if (flags.name) {
+      name = flags.name
+      environments = ['staging', 'production']
+    } else {
+      name = await cli.prompt('What is your project name?')
+      const responses = await inquirer.prompt([{
+        name: 'environment',
+        message: 'choose environments',
+        type: 'checkbox',
+        choices: [{name: 'staging'}, {name: 'production'}],
+      }])
+      environments = responses.environment
+    }
 
     interface Yaml {
       name: string;
@@ -25,17 +38,16 @@ export default class Init extends Command {
     }
 
     const yaml: Yaml = {
-      name: project,
+      name: name,
       environments: {},
     }
 
-    responses.environment.forEach((env: string) => {
+    environments.forEach((env: string) => {
       yaml.environments[env] = {memory: 1024}
     })
-    cli.action.start('Writing fume.yml')
+
+    cli.action.start(`Creating fume.yml for project ${name}`)
     fs.writeFileSync('fume.yml', yml.safeDump(yaml))
     cli.action.stop()
   }
-
-
 }
--- test/commands/init.test.ts
+++ test/commands/init.test.ts
@@ -3,15 +3,8 @@ import {expect, test} from '@oclif/test'
 describe('init', () => {
   test
   .stdout()
-  .command(['init'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
-
-  test
-  .stdout()
-  .command(['init', '--name', 'jeff'])
-  .it('runs init --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
+  .command(['init', '--name', 'bob'])
+  .it('runs init --name bob', ctx => {
+    expect(ctx.stdout).to.contain('')
   })
 })
