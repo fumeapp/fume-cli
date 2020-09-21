3abbb58aafb820eeac933622cb54bd8885c9946a
Kevin Olson <acidjazz@gmail.com>
2020-09-21 02:11:15 -0500

    🚧 general s3 progress

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -1,11 +1,23 @@
-import {Command} from '@oclif/command'
+import Command from '@oclif/command'
 import execa = require('execa')
+import {Observable} from 'rxjs'
 import cli from 'cli-ux'
 import fs = require('fs')
 import archiver  = require('archiver')
 import Listr = require('listr')
+import yml = require('js-yaml')
+import AWS = require('aws-sdk')
 
-export default class Init extends Command {
+interface Config {
+  name: string;
+  environments: Environment;
+}
+
+interface Environment {
+  memory: number;
+}
+
+export default class Deploy extends Command {
   static description = 'Deploy an Environment'
 
   static examples = [
@@ -14,16 +26,23 @@ export default class Init extends Command {
 
   static args = [{name: 'environment', required: true}]
 
+  s3!: AWS.S3
+
+  config!: Config
+
   async run() {
-    const {args: {environment}} = this.parse(Init)
+    const {args: {environment}} = this.parse(Deploy)
 
     if (!fs.existsSync('fume.yml'))
       cli.error('No fume configuration found, please run fume init')
 
+    this.config = yml.load(fs.readFileSync('fume.yml').toString())
+    this.s3 = new AWS.S3()
+
     if (!fs.existsSync('node_modules/.bin/nuxt'))
       cli.error('Nuxt.js binary not found in node_modules/.bin/nuxt')
 
-    this.log(`Deploying ${environment}`)
+    this.log(`Deploying environment ${environment}`)
 
     const tasks = new Listr([
       {
@@ -34,6 +53,10 @@ export default class Init extends Command {
         title: 'Zipping up generated assets',
         task: () => this.archive(environment),
       },
+      {
+        title: 'Uploading deployment package',
+        task: () => this.upload(environment),
+      },
     ])
 
     tasks.run().catch((error: any) => {
@@ -42,21 +65,64 @@ export default class Init extends Command {
     })
   }
 
-  archive(environment: string) {
-    const bar = cli.progress()
-    bar.start()
-    const output = fs.createWriteStream(`deployment-${environment}.zip`)
-    output.on('end', () => this.log('data has been drained'))
-    const archive = archiver('zip', {zlib: {level: 9}})
-    archive.on('error', error => this.error(error))
-    archive.on('progress', progress => {
-      bar.setTotal(progress.entries.total)
-      bar.update(progress.entries.processed)
-      if (progress.entries.total === progress.entries.processed)
-        bar.stop()
+  async upload(environment: string) {
+    const bucket = `fume-${this.config.name}-${environment}`
+    return new Listr([
+      {
+        title: `Checking for bucket ${bucket}`,
+        task: () => {
+          return new Observable(observer => {
+            if (this.exists(bucket)) {
+              observer.next('Bucket found')
+              observer.complete()
+            } else {
+              observer.next(`Creating bucket ${bucket}`)
+              this.s3.createBucket({Bucket: bucket}, (error, data) => {
+                if (error)
+                  this.error(error)
+                this.log(data)
+              })
+            }
+          })
+        },
+      },
+      {
+        title: 'Sending package to bucket',
+        task: () => this.sendFile(bucket),
+      },
+    ])
+  }
+
+  sendFile(bucket: string) {
+    return new Observable(observer => {
+      observer.next(`Sending file to ${bucket}`)
+      setTimeout(() => observer.complete(), 2000)
     })
-    archive.pipe(output)
-    archive.directory('.nuxt/', false)
-    archive.finalize()
+  }
+
+  async exists(bucket: string) {
+    try {
+      await this.s3.headBucket({Bucket: bucket}).promise()
+      return true
+    } catch (error) {
+      return false
+    }
+  }
+
+  archive(environment: string) {
+    return new Observable(observer => {
+      const output = fs.createWriteStream(`deployment-${this.config.name}-${environment}.zip`)
+      output.on('end', () => this.log('data has been drained'))
+      const archive = archiver('zip', {zlib: {level: 9}})
+      archive.on('error', error => this.error(error))
+      archive.on('progress', progress => {
+        observer.next(`${progress.entries.processed * 100 / progress.entries.total}%`)
+        if (progress.entries.total === progress.entries.processed)
+          observer.complete()
+      })
+      archive.pipe(output)
+      archive.directory('dist/', false)
+      archive.finalize()
+    })
   }
 }
