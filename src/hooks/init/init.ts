95c860766585ff9316f23f873c5e3eb31288a46c
tcampbPPU <tcamp022@gmail.com>
2022-04-11 23:44:10 -0400

    run hook to check latest version

--- src/hooks/init/init.ts
+++ src/hooks/init/init.ts
@@ -4,20 +4,20 @@ import axios from 'axios'
 
 /**
  * @desc Fetch the latest dist tag version from the repository
- * @returns Promise<void>
+ * @returns Promise<string|undefined>
  */
-const fetchLatestVersion = async (): Promise<void> => {
+const fetchLatestVersion = async (): Promise<string|undefined> => {
   try {
     const data = (await axios.get('https://registry.npmjs.org/fume-cli')).data
     return data['dist-tags'].latest
   } catch {}
 }
 
-const hook: Hook<'init'> = async function (options: any): Promise<void> {
-  const currentVersion = options.config.version
+const hook: Hook<'init'> = async function (): Promise<void> {
+  const currentVersion = this.config.version
   const latestVersion = await fetchLatestVersion()
 
-  if (currentVersion !== latestVersion) {
+  if (latestVersion && currentVersion !== latestVersion) {
     this.log(chalk.yellow('You are using an old version of the CLI.\n' +
       `You can update it by running: ${chalk.green('yarn global upgrade fume-cli')}`))
   }
